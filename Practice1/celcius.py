fe7040ebb7306411b1e65fcdeaa1948cf239c769
Lance Buenviaje <135933805+Lance-b-Christian@users.noreply.github.com>
2025-05-13 11:27:47 +0800

    Delete Practice1 directory

--- Practice1/celcius.py
+++
@@ -1,3 +0,0 @@
-celcius = (float(input("Enter the value in celsius: ")))
-fahrenheit = (celcius * 9/5) + 32
-print(f"{celcius} C is {fahrenheit} fahrenheit")
--- Practice1/my_tuple = 1, 2, 3.py
+++
@@ -1,7 +0,0 @@
-#concatenation
-
-tuple1 = (10, 29, 23)
-tuple2 = ('i', 'l', 'y')
-concat_tuple = tuple1 + tuple2
-print(concat_tuple)
-
